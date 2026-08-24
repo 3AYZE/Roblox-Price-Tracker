795393edb59276f13e5d68152d2f185bebbeea09
Azy <126979239+3AYZE@users.noreply.github.com>
2026-08-24 00:04:04 -0700

    Add curated GUI global usings

---
+++ src/RobloxPriceTracker.Gui/GlobalUsings.cs
@@ -0,0 +1,26 @@
+// Curated global usings for the mixed WPF + WinForms tray project.
+// Keep System.Drawing and System.Windows.Forms out of global scope to avoid
+// type-name collisions with WPF (Application, Brush, Color, Point, etc.).
+global using System;
+global using System.Collections.Generic;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.Globalization;
+global using System.IO;
+global using System.IO.Compression;
+global using System.Linq;
+global using System.Media;
+global using System.Net.Http;
+global using System.Runtime.CompilerServices;
+global using System.Text;
+global using System.Text.Json;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using System.Windows;
+global using System.Windows.Controls;
+global using System.Windows.Data;
+global using System.Windows.Input;
+global using System.Windows.Media;
+global using System.Windows.Media.Imaging;
+global using System.Windows.Threading;
